897973b8a1ac12f7a9ceb2d5259a672821545c48
jalzae <jalzae@gmail.com>
2024-05-10 15:09:46 +0700

    push fungsi baru

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ function increment(a, b) {
   return a + b
 }
 
+function multiply(a, b) {
+  return a * b
+}
 
-module.exports = { increment }
+
+
+module.exports = { increment,multiply }
--- tests/index.test.js
+++ tests/index.test.js
@@ -3,3 +3,7 @@ const index = require('../index')
 test('add of 1 + 2 to equal 3', () => {
   expect(index.increment(1, 2)).toBe(3);
 });
+
+test('multiply 3*4 to equal 12', () => {
+  expect(index.multiply(3, 4)).toBe(12);
+})
